3a43d7ac2ece5621c84742ce27a5d2741ab2a854
Alex Grant <alex@grantovich.net>
2014-01-28 11:26:15 -0500

    Blats can be created using a form

--- app/controllers/blats_controller.rb
+++ app/controllers/blats_controller.rb
@@ -4,10 +4,25 @@ class BlatsController < ApplicationController
       Blat.all
     else
       Blat.where('updated_at > ?', 7.days.ago)
-    end
+    end.order(updated_at: :desc)
   end
 
   def show
     @blat = Blat.find(params[:id])
+  end
+
+  def new
+    @blat = Blat.new
+  end
+
+  def create
+    @blat = Blat.create(blat_params)
+    redirect_to @blat
+  end
+
+  private
+
+  def blat_params
+    params.require(:blat).permit(:title, :body)
   end
 end
